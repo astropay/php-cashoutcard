29ef035340b090d0baae6406b55948359a28a5c2
Mauricio <mauriciovander@gmail.com>
2015-08-24 12:19:12 -0300

    Create constants.php

---
+++ src/constants.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Astropay;
+
+abstract class Constants
+{
+  static const API_SANDBOX_URL = 'https://sandbox-api.astropaycard.com';
+  static const API_URL = 'https://api.astropaycard.com'
+}
